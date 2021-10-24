15d760c916b9e72dc186fce67791ec9c7638926f
Brian Sletten <brian@bosatsu.net>
2021-10-23 17:30:37 -0700

    Cleaned up slightly.

--- init.el
+++ init.el
@@ -7,8 +7,7 @@
 ;; Package Management
 
 (require 'package)
-(add-to-list
- 'package-archives
+(add-to-list 'package-archives
  '("melpa" . "https://melpa.org/packages/")
  t)
 
@@ -25,7 +24,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
  '(org-agenda-files
-   '("~/Dropbox/org-mode-files/misc.org" "~/Dropbox/org-mode-files/attending.org" "~/Dropbox/org-mode-files/projects.org" "~/Dropbox/org-mode-files/pinboard-bookmarks.org"))
+   '("~/Dropbox/org-mode-files/misc.org"
+     "~/Dropbox/org-mode-files/attending.org"
+     "~/Dropbox/org-mode-files/projects.org"
+     "~/Dropbox/org-mode-files/pinboard-bookmarks.org"))
+ 
  '(package-selected-packages
    '(org)))
